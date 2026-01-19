ca9d8f3f056864135a96763e031d3e42e34eac5a
Jan Kosnar <jan.kosnar.arbeit@gmail.com>
2026-01-19 10:50:08 +0100

    Aufgabe 2
    BMI Werte berechnen bei Frauen und Männern

--- Blockwoche 2/Blockwoche 2/Program.cs
+++ Blockwoche 2/Blockwoche 2/Program.cs
@@ -1,47 +1,75 @@
-﻿
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
+        double Gewicht;
+        double Größe;
+        double BMIE;
+
         string? name;
-        int alter;
+        string? Geschlecht;
 
-        Console.WriteLine($"Hallo Der herr/ Die Dame. Wie heißt du und wie alt bist du?");
-
-        Console.WriteLine("Name: ");
+        Console.WriteLine("Hallo. Wie heißt du?");
         name = Console.ReadLine() ?? "Deafault Benutzer";
+        Console.WriteLine($"Ah, {name}, Wieviel wiegst du?");
+        Gewicht = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Wie Groß bist du?");
+        Größe = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Dein Geschlecht?");
+        Geschlecht = Console.ReadLine();
 
-        if (name.ToLower().Trim() == "Ende")
-        {
-            return;
-        }
+        Geschlecht!.ToLower();
 
-        Console.WriteLine("Alter: ");
-        alter = Convert.ToInt32(Console.ReadLine());
+        BMIE = Rechner(Gewicht, Größe);
 
-        Console.WriteLine($"Hallo {name}, du darfst {1}hinein", Funktion(alter) ? "" : "Nicht ");
+        BMI(BMIE, Geschlecht);
+    }
+    static double Rechner(double Ge, double Gr)
+    {
+        double zwischen1;
+        double ergebnis;
 
-        /* Im prinzip das selbe aber Länger
-         * if (Funktion(alter) == true)
-        {
-            Console.WriteLine($"Du darfst rein {name}.");
-        }
-        else
-        {
-            Console.WriteLine($"Du bist zu Jung {name}.");
-        }
-        */
+        zwischen1 = Gr * Gr;
+        ergebnis = Ge / zwischen1;
+        Math.Round(ergebnis, 2);
+
+        // Console.WriteLine(ergebnis);
+        // Console.WriteLine(zwischen1);
+
+        return ergebnis;
     }
 
-    static bool Funktion(int alter)
+    static void BMI(double bmi, string Gs)
     {
-        return alter >= 18;
-
-        /* Im prinzip das selbe aber Länger
-        if(alter >= 18)
-            return true;
+        if (Gs == "mänlich" || Gs == "mann")
+        {
+            if (bmi < 18.5)
+                Console.WriteLine("Du bist Untergewichtig");
+            else if (bmi >= 18.5 && bmi < 24.9)
+                Console.WriteLine("Du hast das Normalgewicht");
+            else if (bmi >= 25 && bmi < 29.9)
+                Console.WriteLine("Du bist Übergewichtig");
+            else if (bmi >= 30 && bmi < 34.9)
+                Console.WriteLine("Du hast Adipositas Grad I");
+            else if (bmi >= 35 && bmi < 39.9)
+                Console.WriteLine("Du hast Adipositas Grad II");
+            else if (bmi > 40)
+                Console.WriteLine("Du hast Adipositas Grad III");
+        }
         else
-            return false;
-        */
+        {
+            if (bmi < 17.5)
+                Console.WriteLine("Du bist Untergewichtig");
+            else if (bmi >= 17.5 && bmi < 23.9)
+                Console.WriteLine("Du hast das Normalgewicht");
+            else if (bmi >= 24 && bmi < 28.9)
+                Console.WriteLine("Du bist Übergewichtig");
+            else if (bmi >= 29 && bmi < 33.9)
+                Console.WriteLine("Du hast Adipositas Grad I");
+            else if (bmi >= 34 && bmi < 38.9)
+                Console.WriteLine("Du hast Adipositas Grad II");
+            else if (bmi > 39)
+                Console.WriteLine("Du hast Adipositas Grad III");
+        }
     }
 }
