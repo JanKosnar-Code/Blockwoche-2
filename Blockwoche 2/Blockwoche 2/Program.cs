1da63d45e117525a601d7518efe623fa62f60dc8
Jan Kosnar <jan.kosnar.arbeit@gmail.com>
2026-01-19 16:50:29 +0100

    Aufgabe 4 V2
    Kleine verbesserungen am code

--- Blockwoche 2/Blockwoche 2/Program.cs
+++ Blockwoche 2/Blockwoche 2/Program.cs
@@ -1,20 +1,20 @@
-﻿// Namenliste in Array packen mit foreach durch array jedem eintrag random note zuweisen
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
         string? eingabe;
 
-        Console.WriteLine("Namen mit , getrennt eingeben:");
+        Random r = new Random();
+
+        Console.WriteLine("Namen mit  getrennt eingeben:");
         eingabe = Console.ReadLine();
         eingabe!.ToLower();
 
-        string[] Wörter = eingabe.Split(',');
+        string[] Wörter = eingabe.Split(' ');
 
         foreach(string w in Wörter)
         {
-            Random r = new Random();
-            int random = r.Next(0, 6);
+            int random = r.Next(1, 6);
             Console.WriteLine($"Der schüler {w} hat die Note {random} bekommen!");
         }
     }
