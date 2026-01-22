6709acd7a7a554b7c0ec9870fa08ed6dac769358
Jan Kosnar <jan.kosnar.arbeit@gmail.com>
2026-01-22 13:49:14 +0100

    Aufgabe 5 V 3
    KI Fortschritte gemacht, Aussehen und Variablen namen verbessert

--- Blockwoche 2/Blockwoche 2/Program.cs
+++ Blockwoche 2/Blockwoche 2/Program.cs
@@ -48,16 +48,19 @@ class Program
 
     static string Spiel(Eingabe en)
     {
-        string? Input;
+        string? Input; // Player Input
+
+        string EInput; // Enemy Input
 
         bool canParse;
 
-        int Ppoints = 0;
-        int Epoints = 0;
+        int zahl = 0;
 
-        int zahl;
+        int Ppoints = 0; // Player Points
+        int Epoints = 0; // Enempy Points
 
-        Random rnd = new Random();
+        int OPpoints = 0; // Original Player Points
+        int OEpoints = 0; // Original Enemy Points
 
         do
         {
@@ -74,12 +77,16 @@ class Program
                 SpielVariablen SpielVariablen = SpielVariablen.none;
                 canParse = Enum.TryParse(Input, out SpielVariablen);
 
+                Random rnd = new Random(); // Delete Me
+
                 if (!canParse)
                     Console.WriteLine("Fehler, Falsche eingabe!");
                 if (canParse)
                 {
-                    zahl = rnd.Next(1, 6);
-                    string EInput = ((SpielVariablen)zahl).ToString();
+                    //EInput = KI();
+                    zahl = rnd.Next(1, 6); // Delete Me
+
+                    EInput = ((SpielVariablen)zahl).ToString(); // Delete Me
 
                     Console.WriteLine($"Deine eingabe: {Input}, Eingabe des gegners: {EInput}");
 
@@ -97,7 +104,11 @@ class Program
                     else
                         Epoints++;
 
-                    KIRechner(EInput);
+                    KIRechner(EInput, OEpoints, OPpoints, Epoints, Ppoints);
+
+                    OPpoints = Ppoints;  
+                    
+                    OEpoints = Epoints;
 
                     if (Epoints == 3)
                     {
@@ -114,11 +125,53 @@ class Program
         } while (true);
     }
 
-    static void KI()
+    /*static string KI()
     {
+        int zahl;
+        int zahl2;
+        int GrößteZahl;
+        int KleinsteZahl;
 
-    }
-    static void KIRechner(string Ei)
+        string trennzeichen = ", ";
+        string Ei = "";
+
+        Random rnd = new Random();
+
+        string Pfad = "C:\\Users\\Student\\Blockwoche-2\\Informationen\\KIScore.txt";
+        string Inhalt = File.ReadAllText(Pfad);
+        string[] Count = Inhalt.Split(trennzeichen);
+
+        int[] Counting = Array.ConvertAll(Count, int.Parse);
+
+        GrößteZahl = Counting.Max();
+        KleinsteZahl = Counting.Min();
+
+        for (int i = 0; i <= GrößteZahl; i++)
+        {
+            zahl = rnd.Next(0, 5);
+
+            if (Counting[zahl] == KleinsteZahl)
+            {
+                zahl2 = rnd.Next(1, 3);
+                if (zahl2 == 2)
+                {
+                    Ei = ((SpielVariablen)Counting[zahl]).ToString();
+                }
+            }
+            else if (Counting[zahl] == GrößteZahl)
+            {
+                Ei = ((SpielVariablen)Counting[zahl]).ToString();
+                return (Ei);
+            }
+            else
+            {
+                Ei = ((SpielVariablen)Counting[zahl]).ToString();
+            }
+        }
+        return Ei;
+    }*/
+
+    static void KIRechner(string Ei, int OEP, int OPP, int EP, int PP)
     {
         int schere;
         int stein;
@@ -126,17 +179,48 @@ class Program
         int echse;
         int spock;
 
-        char[] trennzeichen = { ' ', ',' };
+        string trennzeichen = ", ";
 
         string Pfad = "C:\\Users\\Student\\Blockwoche-2\\Informationen\\KIScore.txt";
         string Inhalt = File.ReadAllText(Pfad);
 
+        string NewCount;
+
         string[] Count = Inhalt.Split(trennzeichen);
 
-        foreach (string s in Count)
+        schere = Convert.ToInt32(Count[0]); stein = Convert.ToInt32(Count[1]); papier = Convert.ToInt32(Count[2]);
+        echse = Convert.ToInt32(Count[3]); spock = Convert.ToInt32(Count[4]);
+
+        if (EP > OEP) 
         {
-            Console.WriteLine(s);
+            if (Ei == "schere")
+                schere += 1;
+            else if (Ei == "stein")
+                stein += 1;
+            else if (Ei == "papier")
+                papier += 1;
+            else if (Ei == "echse")
+                echse += 1;
+            else if (Ei == "spock")
+                spock += 1;
         }
+        else if (PP > OPP)
+        {
+            if (Ei == "schere")
+                schere -= 1;
+            else if (Ei == "stein")
+                stein -= 1;
+            else if (Ei == "papier")
+                papier -= 1;
+            else if (Ei == "echse")
+                echse -= 1;
+            else if (Ei == "spock")
+                spock -= 1;
+        }
+
+        NewCount = $"{schere}, {stein}, {papier}, {echse}, {spock}";
+
+        File.WriteAllText(Pfad, NewCount);
     }
 
     enum Eingabe
